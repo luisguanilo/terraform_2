b182c8aa81d580babc9befcd986e27ea2b12784c
luisguanilo <guanilo99@gmail.com>
2025-04-19 14:55:12 -0500

    definimos la variables a usar

---
+++ variables.tf
@@ -0,0 +1,8 @@
+# En este archivo se definen las variables 
+
+variable "web_server_port" {
+  description = "Puerto de entrada del servidor web"
+  type        = number  # asignamos el tipo de la variable como numerico
+  default     = 4007  # Se especifica el número de 4007
+}
+
